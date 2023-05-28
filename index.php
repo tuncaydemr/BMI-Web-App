3052503d50ce8708031a1f8a41fd3afc8ad49036
Tuncay Demir <tuncaydemir682@gmail.com>
2023-05-28 18:48:52 +0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
+</head>
+
+<body>
+  <div class="container my-3">
+    <div class="card mb-3">
+      <div class="row">
+        <div class="col-3">
+          <img src="img/2.jpg" alt="2" class="img-fluid rounded-start">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title">PHP Course</h5>
+            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt adipisci cumque cum maxime enim ipsum sed vitae odit eaque eius!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
