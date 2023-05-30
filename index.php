3059ee4686d47b57b7dcb9d2f12a59cd18f1629d
Fogo Software <tuncaydemir682@gmail.com>
2023-05-30 17:17:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="./src/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
 </head>
 
 <body>
   <form action="index.php" method="post">
-    <div class="container d-flex align-items-center justify-content-center">
+    <div class="container d-flex align-items-center justify-content-center" style="height: 100vh;">
       <div class="card w-50">
-        <h4 class="my-5 text-center">Body Mass Calculator</h4>
+        <h3 class="my-5 text-center">Body Mass Calculator</h3>
         <div class="mb-4 w-50 m-auto">
           <label for="weight" class="form-label">Weight</label>
           <input type="text" class="form-control" name="weight" id="weight" placeholder="Enter Your Weight" required>
@@ -23,8 +22,8 @@
           <label for="height" class="form-label">Height</label>
           <input type="text" class="form-control" name="height" id="height" placeholder="Enter Your Height" required>
         </div>
-        <div class="d-flex justify-content-center">
-          <button type="button" name="button" id="button" class="btn btn-primary w-50">Button</button>
+        <div class="d-flex justify-content-center mt-3 mb-5">
+          <button type="button" name="button" id="button" class="btn btn-primary w-50 my-4">Button</button>
         </div>
       </div>
     </div>
