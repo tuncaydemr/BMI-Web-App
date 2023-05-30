2083111e557ce706a51f96cbceeac6be42d01de0
Fogo Software <tuncaydemir682@gmail.com>
2023-05-30 16:41:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,19 +6,18 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
   <link rel="stylesheet" href="./src/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
 </head>
 
 <body>
   <form>
     <div class="container d-flex align-items-center justify-content-center">
-      <div class="card">
-        <h4 class="mt-3 mb-5">Body Mass Calculator</h4>
+      <div class="card d-flex justify-content-center">
+        <h4 class="mt-3 mb-5 text-center">Body Mass Calculator</h4>
         <div class="mb-3 w-75">
-          <label for="" class="form-label">Email</label>
-          <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="abc@mail.com">
-          <small id="emailHelpId" class="form-text text-muted">Help text</small>
+          <label for="" class="form-label">Weight</label>
+          <input type="text" class="form-control" name="" id="" placeholder="Enter Your Weight">
         </div>
       </div>
     </div>
