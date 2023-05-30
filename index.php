fb79da2e1f41ea1e4e915edd76f5dbf1c94b4ad3
Fogo Software <tuncaydemir682@gmail.com>
2023-05-30 19:45:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,11 +27,11 @@
         <h3 class="my-5 text-center">Body Mass Calculator</h3>
         <div class="mb-4 w-50 m-auto">
           <label for="weight" class="form-label">Weight</label>
-          <input type="text" class="form-control" name="weight" id="weight" pattern="\d{3}" placeholder="Enter Your Weight" value="<?php echo isset($weight) ? $weight : ''; ?>">
+          <input type="text" class="form-control" name="weight" id="weight" pattern="\d{3}" placeholder="Enter Your Weight" value="<?php echo $weight; ?>">
         </div>
         <div class="mb-4 w-50 m-auto">
           <label for="height" class="form-label">Height</label>
-          <input type="text" class="form-control" name="height" id="height" pattern="\d{3}" placeholder="Enter Your Height" value="<?php echo isset($height) ? $height : ''; ?>">
+          <input type="text" class="form-control" name="height" id="height" pattern="\d{3}" placeholder="Enter Your Height" value="<?php echo $height; ?>">
         </div>
         <div class="mb-4 w-50 m-auto">
           <label for="bmi" class="form-label">BMI</label>
