1cce36b0fc9bd2b908ea53f85450133742b9a0e4
Fogo Software <tuncaydemir682@gmail.com>
2023-05-30 16:13:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,24 +7,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="./src/style.css">
 </head>
 
 <body>
-  <div class="container my-3">
-    <div class="card mb-3">
-      <div class="row">
-        <div class="col-3">
-          <img src="img/2.jpg" alt="2" class="img-fluid rounded-start">
-        </div>
-        <div class="col-9">
-          <div class="card-body">
-            <h5 class="card-title">PHP Course</h5>
-            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt adipisci cumque cum maxime enim ipsum sed vitae odit eaque eius!</p>
-          </div>
-        </div>
+  <form>
+    <div class="container d-flex align-items-center my-3">
+      <div class="card text-center">
+        <h4>Body Mass Calculator</h4>
       </div>
     </div>
-  </div>
+  </form>
 </body>
 
 </html>
