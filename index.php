bba6f22090fa98d35cddac0534f175da34c7296a
Fogo Software <tuncaydemir682@gmail.com>
2023-05-30 17:25:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Body Mass Calculator</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
 </head>
 
 <body style="background: black;">
-  <form action="index.php" method="post">
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <div class="container d-flex align-items-center justify-content-center" style="height: 100vh;">
       <div class="card w-50">
         <h3 class="my-5 text-center">Body Mass Calculator</h3>
@@ -23,7 +23,7 @@
           <input type="text" class="form-control" name="height" id="height" placeholder="Enter Your Height" required>
         </div>
         <div class="d-flex justify-content-center mt-3 mb-5">
-          <button type="button" name="button" id="button" class="btn btn-primary w-50 my-4">Button</button>
+          <button type="submit" name="submit" id="submit" class="btn btn-primary w-50 my-4">Button</button>
         </div>
       </div>
     </div>
