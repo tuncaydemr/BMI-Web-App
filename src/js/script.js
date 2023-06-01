54e3cd07efc351479d2b5d506ca70dd881c8fc6e
Fogo Software <tuncaydemir682@gmail.com>
2023-06-01 21:24:20 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,15 @@ $(() => {
   let $weight = $('#weight').val();
   let $height = $('#height').val();
 
-
-    $('#weight, #height').keyup(() => {
-      if (($weight != "") || ($height != "")) {
-        $('button').addClass('disabled');
-        $('button').removeClass('enabled');
-      } else {
-        $('button').addClass('enabled');
-        $('button').removeClass('disabled');
-      }
-    });
-
+  $('#weight, #height').keyup(() => {
+    if (($weight != "") || ($height != "")) {
+      $('button').addClass('disabled');
+      $('button').removeClass('enabled');
+    } else {
+      $('button').addClass('enabled');
+      $('button').removeClass('disabled');
+    }
+  });
 
   if (($weight != "") && ($height != "")) {
     $('.bmi').addClass('d-block');
