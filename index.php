9d0cbdb0cb1a3784b0f0c2b97c2d5876247b3b23
Fogo Software <tuncaydemir682@gmail.com>
2023-06-01 21:52:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
         endif;
         ?>
         <div class="d-flex justify-content-center mt-3 mb-5">
-          <button type="submit" name="submit" id="submit" class="btn btn-primary w-50 my-4 py-3" disabled>Submit</button>
+          <button type="submit" name="submit" id="submit" class="btn btn-primary w-50 my-4 py-3">Submit</button>
         </div>
       </div>
     </div>
